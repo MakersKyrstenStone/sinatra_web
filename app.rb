201358c264314ca48c1503ec81cb6853d3d50707
Kyrsten <Kyrsten>
2022-02-02 16:57:45 +0000

    added styles to web page

--- app.rb
+++ app.rb
@@ -5,10 +5,13 @@ get '/' do
   "This is Weird"
 end
 get '/secret' do
-  "I'm Hiding from you"
+  "<div style='border: 6px solid green'>
+  <p style='border: 3px dashed purple' >
+  I'm colourful and a bit more interesting </p>
+  </div>"
 end
 get '/cat' do
-  "<div>
-  <img src='https://i.imgur.com/jFaSxym.png'>
-  <div>"
+  "<div style='border: 3px solid green'>
+  <img style='border: 3px dashed red' src='https://i.imgur.com/jFaSxym.png' >
+  </div>"
 end
