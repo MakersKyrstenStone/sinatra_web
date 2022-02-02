2f6caead92c48b568d020e454e6082b93e21d257
Kyrsten <Kyrsten>
2022-02-02 15:57:36 +0000

    added a cat pic to webpage

--- app.rb
+++ app.rb
@@ -4,3 +4,11 @@ require "sinatra/reloader" if development?
 get '/' do
   "This is Weird"
 end
+get '/secret' do
+  "I'm Hiding from you"
+end
+get '/cat' do
+  "<div>
+  <img src='https://i.imgur.com/jFaSxym.png'>
+  <div>"
+end
