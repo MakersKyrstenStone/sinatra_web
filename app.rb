4b4f1aef603fcd8d2b785b29161eeb8fc9e183ac
Kyrsten <Kyrsten>
2022-02-03 15:05:19 +0000

    text and cat pic - erb file

--- app.rb
+++ app.rb
@@ -4,14 +4,11 @@ require "sinatra/reloader" if development?
 get '/' do
   "This is Weird"
 end
+
 get '/secret' do
-  "<div style='border: 6px solid green'>
-  <p style='border: 3px dashed purple' >
-  I'm colourful and a bit more interesting </p>
-  </div>"
+  erb(:secret)
 end
+
 get '/cat' do
-  "<div style='border: 3px solid green'>
-  <img style='border: 3px dashed red' src='https://i.imgur.com/jFaSxym.png' >
-  </div>"
+ erb(:index)
 end
